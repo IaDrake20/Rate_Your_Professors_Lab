83d61344257d70971b63aea9cb3dfd425b3986ef
IaDrake20 <ios.drake05@gmail.com>
2024-01-31 10:06:16 -0500

    added very basic stuff to read from cmd line, will fill out more as day goes

--- Lab_RateMyProfessor/Program.cs
+++ Lab_RateMyProfessor/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Please enter the Professor's name: ");
+string p_input = Console.ReadLine();
+//handle if input is null, not alphabetic
+
+//check if Professor exists
+    //if so, do not make new ID
+
+Console.WriteLine("Please enter your rating of the Professor: ");
+string r_input = Console.ReadLine();
+//handle if input is null, not a number, a number > 10 or < 0
+
+
