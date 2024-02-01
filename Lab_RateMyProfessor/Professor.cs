56f03d3d20eb358e3f62def3a4abb4b58bba470e
uglyrobo <63438682+uglyrobo@users.noreply.github.com>
2024-02-01 05:08:38 -0500

    Update Professor.cs

--- Lab_RateMyProfessor/Professor.cs
+++ Lab_RateMyProfessor/Professor.cs
@@ -9,6 +9,7 @@ namespace RateMyProfessor
 
 		List<Rating> ratings;
 
+		static String[] internallisting=new String[200];
 
 		public Professor(String n)
 		{
@@ -17,6 +18,11 @@ namespace RateMyProfessor
 			id = Guid.NewGuid();
 
 			ratings = new List<Rating>();
+			for(int i = 1; i < internallisting.Length; i++)
+				{
+				if (internallisting[i].Equals(null))
+					internallisting[i] = name;
+				}
 		}
 
 
@@ -25,8 +31,60 @@ namespace RateMyProfessor
 			return id;
 		}
 
+		public void addProfRating(Rating r)
+		{
+			ratings.Add(r);
+		}
 
+        public String deleteProfRating(Rating r)
+        {
+			if (ratings.Contains(r))
+			{
+				ratings.Remove(r);
+				return "Successfully deleted rating: " + r;
+			}
+			else
+				return "Could not remove rating";
+		   
+        }
 
+        public int getProfessorpos(String name)
+		{
+			int pos = 0;
+			int i = 1;
+			while (!internallisting[i++].Equals(name))
+
+				
+				if (i != internallisting.Length)
+					pos = i;
+				else
+					i = 1;
+			return pos;
+		}
+		public void deleteProfessorpos(int pos)
+		{
+			for (int i = pos; i < internallisting.Length; i++)
+			{
+
+				if (internallisting[i].Equals(null)||i == internallisting.Length-1)
+				{
+					for(int j = pos; j < i; j++)
+					{
+						internallisting[j] = internallisting[j + 1];
+					}
+				}
+					
+			}
+        }
+		public void deleteProf()
+		{
+            int p=getProfessorpos(name);
+			deleteProfessorpos(p);
+            name = "";
+			id = Guid.Empty;
+			ratings = new List<Rating>(); 
+			
+		}
 	}
 }
 
