c7e1a2990d1db2e25f67add4d238989f5c727a3e
uglyrobo <63438682+uglyrobo@users.noreply.github.com>
2024-02-02 11:21:59 -0500

    Update Program.cs

--- Lab_RateMyProfessor/Program.cs
+++ Lab_RateMyProfessor/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using RateMyProfessor;
+
 Console.WriteLine("Hello, World!");
+Professor.internallisting
+/*
+ * Planned method for if you want to find a prof name within the array and don't want to write out the name
+ * 
+ */
