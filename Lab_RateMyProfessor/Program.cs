efc61122c4d41726a9ae8dcbb6eb3bfc55352890
IaDrake20 <ios.drake05@gmail.com>
2024-02-01 20:13:28 -0500

    added test stuff

--- Lab_RateMyProfessor/Program.cs
+++ Lab_RateMyProfessor/Program.cs
@@ -1,13 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Please enter the Professor's name: ");
-string p_input = Console.ReadLine();
-//handle if input is null, not alphabetic
+﻿using System;
 
-//check if Professor exists
-    //if so, do not make new ID
+namespace RateMyProfessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram  = new Program();
+            var name = myProgram.RecieveProfName();
+            // handle if input is null, not alphabetic
 
-Console.WriteLine("Please enter your rating of the Professor: ");
-string r_input = Console.ReadLine();
-//handle if input is null, not a number, a number > 10 or < 0
+            // check if Professor exists
+            // if so, do not make a new ID
 
+            
 
+            // handle if input is null, not a number, a number > 10 or < 0
+
+            // You can call other methods or perform additional actions here as needed
+        }
+
+        public string RecieveProfName()
+        {
+            Console.WriteLine("Please enter the Professor's name: ");
+            string p_input = Console.ReadLine();
+            return p_input;
+        }
+
+        public string RecieveProfRating()
+        {
+            Console.WriteLine("Please enter your rating of the Professor: ");
+            string r_input = Console.ReadLine();
+            return r_input;
+        }
+    }
+}
--- Lab_RateMyProfessor/Program_Test.cs
+++ Lab_RateMyProfessor/Program_Test.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using RateMyProfessor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using Xunit;
 
 namespace Lab_RateMyProfessor
 {
-    internal class Program_Test
+    public class Program_Test
     {
+        [Fact]
+        public void ValidProfName()
+        {
+            var prog = new Program();
+
+            var result = prog.RecieveProfName();
+
+            //check name is only letters
+            Assert.True(result.All(char.IsLetter));
+
+            //check that name exists
+            Assert.True(result.Length > 0);
+        }
+
+        [Fact]
+        public void IsValidRating_ValidRating_ReturnsTrue()
+        {
+            var prog = new Program();
+
+            var result = prog.RecieveProfName();
+
+            // Assert
+            Assert.True(result);
+        }
     }
 }
