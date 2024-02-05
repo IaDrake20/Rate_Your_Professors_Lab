d327da71a1566e6ae1f2f96e47f9160c96851e70
uglyrobo <63438682+uglyrobo@users.noreply.github.com>
2024-02-05 05:22:11 -0500

    Update Program.cs

--- Lab_RateMyProfessor/Program.cs
+++ Lab_RateMyProfessor/Program.cs
@@ -3,100 +3,122 @@ using System;
 using System.ComponentModel.Design;
 using System.Reflection.Metadata.Ecma335;
 using Xunit;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace RateMyProfessor
 {
     class Program
     {
 
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             bool run = true;
             string response = "";
             string prof_name = "";
 
-            string _guid = "";
-
             string cat_desc = "";
-            Category _category = new Category();
+            Category _cat = new Category();
             Professor _prof = new Professor();
 
-            
+
 
             while (run)
             {
-                Console.WriteLine("\n\nM: M for menu to see options, E: Exit," +
+                Console.WriteLine("M: M for menu to see options, E: Exit, " +
                             "\n-add: start adding a professor to be rated within a category, -addProf: add professor only, -addCateg: add category only, -addRating: add Rating to a professor" +
-                            "\n-editProf: edit name, -editCateg: edit the category name and description, -editRating: edit the rating of a professor" +
-                            "\n-viewProf: view a list of professors and their Guids, -viewCateg: view list of categories, -viewRatings: view list of ratings" +
-                            "\n-rmProf: remove a professor, -rmCateg: remove a category, -rmRating: remove a rating from a professor" +
-                            "\n-delProf: delete professor json file, -delCateg: delete category json file, -delRating: delete ratings file" +
+                            "\n-editProf: edit name, -editCateg: edit the category name and description, -editRateing: edit the rating of a professor" +
                             "\n");
                 response = Console.ReadLine();
 
                 switch (response)
                 {
                     case "M":
-                        Console.WriteLine("\n\nM: M for menu to see options, E: Exit," +
+                        Console.WriteLine("M: M for menu to see options, E: Exit, C: Continue, " +
                             "\n-add: start adding a professor to be rated within a category, -addProf: add professor only, -addCateg: add category only, -addRating: add Rating to a professor" +
-                            "\n-editProf: edit name, -editCateg: edit the category name and description, -editRating: edit the rating of a professor" +
-                            "\n-viewProf: view a list of professors and their Guids, -viewCateg: view list of categories, -viewRatings: view list of ratings"+
-                            "\n-rmProf: remove a professor, -rmCateg: remove a category, -rmRating: remove a rating from a professor" +
-                            "\n-delProf: delete professor json file, -delCateg: delete category json file, -delRating: delete ratings file" +
+                            "\n-editProf: edit name, -editCateg: edit the category name and description, -editRateing: edit the rating of a professor" +
                             "\n");
                         break;
                     case "-add":
                         Console.WriteLine("Please enter a professor's name.");
                         prof_name = Console.ReadLine();
                         _prof = new Professor(prof_name);
-                        File_Manager.addProfessor(_prof);
 
                         Console.WriteLine("Please enter a category.");
                         string cat_name = Console.ReadLine();
-                        
 
                         Console.WriteLine("Please enter the category's description.");
-                        cat_desc = Console.ReadLine();
-                        _category = new Category(cat_name, cat_desc);
-                        File_Manager.addCategory(_category);    
+                        prof_name = Console.ReadLine();
+                        Category _category = new Category(cat_name, cat_desc);
 
                         Console.WriteLine("Please enter a rating for the professor.");
                         string prof_rating = Console.ReadLine();
-                        
-
-                        try
-                        {
-                            int rating = Int32.Parse(prof_rating);
-                        } catch( Exception ex)
-                        {
-                            Console.WriteLine("ERROR: Please enter a number for the rating.");
-                            break;
-                        }
-
                         Ratings _rating = new Ratings(_prof.getId(), _category.getCategoryId(), Int32.Parse(prof_rating));
-                        _prof.addRating(_rating);
-                        File_Manager.addRating(_rating);
                         break;
                     case "-addProf":
                         Console.WriteLine("Please enter a professor's name.");
                         prof_name = Console.ReadLine();
                         _prof = new Professor(prof_name);
-                        File_Manager.addProfessor(_prof);
                         break;
                     case "-addCateg":
                         Console.WriteLine("Please enter a category.");
                         cat_name = Console.ReadLine();
 
                         Console.WriteLine("Please enter the category's description.");
-                        response = Console.ReadLine();
+                        prof_name = Console.ReadLine();
                         _category = new Category(cat_name, cat_desc);
+<<<<<<< Updated upstream
+=======
                         File_Manager.addCategory(_category);
                         break;
                     case "-addRating":
-                        Console.WriteLine("Please enter the rating to add.");
-                        prof_rating = Console.ReadLine();
-                        _rating = new Ratings(_prof.getId(), _category.getCategoryId(), Int32.Parse(prof_rating));
-                        File_Manager.addRating(_rating);
+                        
+                        Professor prof2 = new Professor();
+                        Category cat = new Category();
+                        Console.WriteLine("Please enter the Guid of the professor to add the Rating to");
+                        _guid = Console.ReadLine();
+                        
+                        List<Professor> _professors2 = File_Manager.getProfessors();
+                        foreach (Professor p in _professors2)
+                        {
+                            if (p.id.ToString().Equals(_guid))
+                            {
+                                // Console.WriteLine("Comparing " + p.id + " to " + _guid);
+                                prof2 = p;
+                                Console.WriteLine("Please enter the category to add the Rating to.");
+                                _guid = Console.ReadLine();
+                                List<Category> cate = File_Manager.getCategories();
+                                foreach (Category c in cate)
+                                {
+                                    if (c.categoryId.ToString().Equals(_guid))
+                                    {
+                                        cat= c;
+                                        
+                                        Console.WriteLine("Please enter the rating to add.");
+                                        prof_rating = Console.ReadLine();
+                                        _rating = new Ratings(prof2.getId(), cat.getCategoryId(), Int32.Parse(prof_rating));
+                                        while (_rating.ratingValue < 1 || _rating.ratingValue > 10)
+                                        {
+                                            Console.WriteLine("Please enter the rating to add.");
+                                            prof_rating = Console.ReadLine();
+                                            _rating = new Ratings(prof2.getId(), cat.getCategoryId(), Int32.Parse(prof_rating));
+                                        }
+                                        File_Manager.addRating(_rating);
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("ERROR: category not found. Check your Guid");
+                                        break;
+                                    }
+                                }
+                                
+                            }
+                            else
+                            {
+                                Console.WriteLine("ERROR: professor not found. Check your Guid");
+                                break;
+                            }
+                        }
+
                         break;
                     case "-editProf":
                         Console.WriteLine("Please enter the Guid of the professor to edit");
@@ -148,8 +170,98 @@ namespace RateMyProfessor
                         }
                         break;
                     case "-editCateg":
+                        Category newcat = new Category();
+                        Console.WriteLine("Please enter the category to add the Rating to.");
+                        _guid = Console.ReadLine();
+                        List<Category> catee = File_Manager.getCategories();
+                        foreach (Category c in catee)
+                        {
+                            if (c.categoryId.ToString().Equals(_guid))
+                            {
+                                newcat = c;
+                                Guid ggggg = newcat.getCategoryId();
+                                File_Manager.deleteCategory(newcat);
+                                Console.WriteLine("Please enter the new category name.");
+                                string name=Console.ReadLine();
+                                Console.WriteLine("Please enter the new category description.");
+                                string desc=Console.ReadLine();
+                                Category arisencategory=new Category(ggggg);
+                                File_Manager.addCategory(arisencategory);
+                                arisencategory.setName(name);   
+                                arisencategory.setDescription(desc);
+                            }
+                            else
+                            {
+                                Console.WriteLine("ERROR: category not found. Check your Guid");
+                                break;
+                            }
+                        }
                         break;
                     case "-editRating":
+                        Professor prof22 = new Professor();
+                        Category cat22 = new Category();
+                        Console.WriteLine("Please enter the Guid of the professor to change the Rating to");
+                        _guid = Console.ReadLine();
+
+                        List<Professor> _professors22 = File_Manager.getProfessors();
+                        foreach (Professor p22 in _professors22)
+                        {
+                            if (p22.id.ToString().Equals(_guid))
+                            {
+                                // Console.WriteLine("Comparing " + p22.id + " to " + _guid);
+                                prof22 = p22;
+                                Console.WriteLine("Please enter the category to change the Rating to.");
+                                _guid = Console.ReadLine();
+                                List<Category> cate22 = File_Manager.getCategories();
+                                foreach (Category c22 in cate22)
+                                {
+                                    if (c22.categoryId.ToString().Equals(_guid))
+                                    {
+                                        cat22 = c22;
+
+                                        Console.WriteLine("Please enter the guid of the rating to change.");
+                                        string ratid = Console.ReadLine();
+                                        List<Ratings>ratatat= prof22.getRat();
+                                        foreach (Ratings r22 in ratatat)
+                                        {
+                                            if (r22.categoryId.ToString().Equals(ratid))
+                                            {
+                                                Console.WriteLine("Please enter the new rating.");
+                                                string newrat = Console.ReadLine();
+                                                _rating = new Ratings(prof22.getId(), cat22.getCategoryId(), Int32.Parse(newrat));
+                                                while (_rating.ratingValue < 1 || _rating.ratingValue > 10)
+                                                {
+                                                    Console.WriteLine("Please enter the new rating.");
+                                                    newrat = Console.ReadLine();
+                                                    _rating = new Ratings(prof22.getId(), cat22.getCategoryId(), Int32.Parse(newrat));
+
+                                                }
+                                                File_Manager.addRating(_rating);
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("ERROR: rating not found. Check your Guid");
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("ERROR: category not found. Check your Guid");
+                                        break;
+                                    }
+                                }
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("ERROR: professor not found. Check your Guid");
+                                break;
+                            }
+                        }
+
+                        break;
+
                         break;
                     case "-rmProf":
                         Console.WriteLine("Please enter the Guid of the professor to remove");
@@ -173,17 +285,17 @@ namespace RateMyProfessor
                         Console.WriteLine("Please enter the Guid of the category to remove.");
                         _guid = Console.ReadLine();
 
-                        Category cat = new Category();
+                        Category catel = new Category();
 
                         List<Category> _categories = File_Manager.getCategories();
                         foreach(Category c in _categories)
                         {
                             if(c.categoryId.ToString().Equals(_guid))
                             {
-                                cat = c;
+                                catel = c;
                             }
                         }
-                        File_Manager.deleteCategory(cat);
+                        File_Manager.deleteCategory(catel);
                         break;
                     case "-rmRating":
                         Console.WriteLine("Please enter the Guid of the rating to remove.");
@@ -231,12 +343,13 @@ namespace RateMyProfessor
                         break;
                     case "delRatings":
                         File_Manager.DeleteFile("ratings.json");
+>>>>>>> Stashed changes
                         break;
                     case "E":
                         Console.WriteLine("Exiting Program...");
                         run = false;
                         break;
-                }    
+                }
             }
         }
     }
